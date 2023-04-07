36701d38f99738c2c485de4bef20f4131e028700
eduarchilon <eduarchilon@outlook.es>
2023-04-06 22:29:17 -0300

    fix(app): change route src files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const dbConnect = require('./config/mongodb')
+const dbConnect = require('./src/config/mongodb')
 
 const app = express()
 
@@ -10,7 +10,7 @@ app.use(express.json())
 
 const port = process.env.PORT || 3000
 
-app.use('/ropaviajero', require('./routes/index.js'))
+app.use('/ropaviajero', require('./src/routes/index.js'))
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
